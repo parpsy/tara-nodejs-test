4ca8bf4283ce5853170ed68f50461c5b113eb9d0
Parsa Dehghan <parsa.tehrankids@gmail.com>
2023-08-16 19:35:09 +0330

    chore: setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "no-console": "off",
+    "consistent-return": "off",
+  },
+};
